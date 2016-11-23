868020c7610bce18b4dd5b64c6a95f5e56d89bf4
shayuna <shaiz2206@gmail.com>
2016-11-23 07:57:52 +0200

    v1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pdf1 = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.pdf1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pdf1
+            // 
+            this.pdf1.Enabled = true;
+            this.pdf1.Location = new System.Drawing.Point(-14, 1);
+            this.pdf1.Name = "pdf1";
+            this.pdf1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("pdf1.OcxState")));
+            this.pdf1.Size = new System.Drawing.Size(1127, 453);
+            this.pdf1.TabIndex = 0;
+            this.pdf1.Enter += new System.EventHandler(this.axAcroPDF1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 455);
+            this.Controls.Add(this.pdf1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pdf1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAcroPDFLib.AxAcroPDF pdf1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://www.otorita.net/otoritadb/pages/hozim_new/103_1479.pdf");
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+
+            byte[] responseBytes = new BinaryReader(response.GetResponseStream()).ReadBytes((int)response.ContentLength);
+                        System.IO.File.WriteAllBytes("c:\\users\\shay\\currentPDF.pdf", responseBytes);
+  //                      MessageBox.Show(responseString);
+//            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private void axAcroPDF1_Enter(object sender, EventArgs e)
+        {
+            pdf1.LoadFile("c:\\users\\shay\\currentPDF.pdf");
+//            System.IO.File.Delete("c:\\users\\shay\\currentPDF.pdf");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Form1.ActiveForm.Close();
+        }
+    }
+}
